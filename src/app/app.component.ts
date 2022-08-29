c15424f690379840f42f09871ec876144ac9f98d
Chilliane <basilioguevarra@gmail.com>
2022-08-29 23:15:49 +0800

    Personal project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'exercise-app';
+export class AppComponent implements OnInit{
+  tittle = "Single Application"
+  constructor(private http: HttpClient){}
+  apidata:any[]=[];
+  loadData(){
+    this.http.get('https://public.connectnow.org.uk/applicant-test/').subscribe((apidata:any)=>{
+    this.apidata = apidata;
+    });
+  }
+  ngOnInit(): void {
+    this.loadData();
+  }
 }
